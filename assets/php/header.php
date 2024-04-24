f0af0b9b9cfc3bc4427525ed09ac1cc9a9a9b492
nxuanhoa9003 <ngohoa0908@gmail.com>
2024-04-24 21:18:30 +0700

    up_1

--- assets/php/header.php
+++ assets/php/header.php
@@ -1,7 +1,7 @@
 <header class="bg-white px-3 py-2 d-flex flex-wrap justify-content-between align-items-center gap-2 position-sticky top-0">
 	<a class="d-flex justify-content-center align-items-end gap-2 text-center link-dark link-underline-opacity-0" href="/do-an-1/test">
 		<div class="logo rounded-circle"></div>
-		<div class="m-0 h5">Trường Đại học GOATS</div>
+		<div class="m-0 h5 fw-medium">Trường Đại Học GOATS</div>
 	</a>
 	<div class="btn-group">
 		<div type="button" class="dropdown-toggle d-flex gap-2 align-items-center" data-bs-toggle="dropdown" aria-expanded="false">
@@ -9,8 +9,12 @@
 			Tên User
 		</div>
 		<ul class="dropdown-menu dropdown-menu-end">
-			<li><div class="dropdown-item">Đổi mật khẩu</div></li>
-			<li><div class="dropdown-item">Đăng xuất</div></li>
+			<li>
+				<div class="dropdown-item">Đổi mật khẩu</div>
+			</li>
+			<li>
+				<div class="dropdown-item">Đăng xuất</div>
+			</li>
 		</ul>
 	</div>
 </header>
--- assets/php/sidebar.php
+++ assets/php/sidebar.php
@@ -1,8 +1,8 @@
-<div class="sidebar bg-white py-3 d-flex flex-column gap-2">
-	<a class="here link-dark link-underline-opacity-0" href="/do-an-1/test">Chương trình khung</a>
-	<a class="link-dark link-underline-opacity-0" href="/do-an-1/test">Quản lý lớp danh nghĩa</a>
-	<a class="link-dark link-underline-opacity-0" href="/do-an-1/test">Lịch theo tiến độ</a>
-	<a class="link-dark link-underline-opacity-0" href="/do-an-1/test">Điểm rèn luyện</a>
-	<a class="link-dark link-underline-opacity-0" href="/do-an-1/test">Điểm thi</a>
-	<a class="link-dark link-underline-opacity-0" href="/do-an-1/test">Điểm quá trình</a>
+<div class="sidebar bg-white py-4 d-flex flex-column gap-2">
+	<a class="here link-dark link-underline-opacity-0 fw-semibold px-3" href="/do-an-1/test">Chương trình khung</a>
+	<a class="link-dark link-underline-opacity-0 fw-semibold px-3" href="/do-an-1/test">Quản lý lớp danh nghĩa</a>
+	<a class="link-dark link-underline-opacity-0 fw-semibold px-3" href="/do-an-1/test">Lịch theo tiến độ</a>
+	<a class="link-dark link-underline-opacity-0 fw-semibold px-3" href="/do-an-1/test">Điểm rèn luyện</a>
+	<a class="link-dark link-underline-opacity-0 fw-semibold px-3" href="/do-an-1/test">Điểm thi</a>
+	<a class="link-dark link-underline-opacity-0 fw-semibold px-3" href="/do-an-1/test">Điểm quá trình</a>
 </div>
