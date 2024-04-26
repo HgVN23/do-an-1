2addd4f66159cea8804c21f0949142170622fe34
Shuuya <enderhoang@gmail.com>
2024-04-26 23:04:57 +0700

    Full Khoa

---
+++ khoa/sidebar.php
@@ -0,0 +1,4 @@
+<div class="sidebar bg-white py-4 d-flex flex-column gap-2">
+	<a class="link-dark link-underline-opacity-0 fw-semibold px-3" href="chuong-trinh-khung.php">Chương trình khung</a>
+	<a class="link-dark link-underline-opacity-0 fw-semibold px-3" href="danh-sach-giang-vien.php">Danh sách giảng viên</a>
+</div>
